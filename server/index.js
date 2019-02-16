47da79ef9d0951e16c76790d0af7ee6ff5bd160b
Daniel Ale <danielale9291@gmail.com>
2019-02-16 10:58:43 +0100

    Fix index js

--- server/index.js
+++ server/index.js
@@ -13,48 +13,43 @@ const productsController = require("./controllers/products_controller");
 const app = express();
 const PORT = 5000;
 
-mongoose.connect(process.env.CONNECTION_STRING, { useNewUrlParser: true }, (err) => {
-	if (err) {
-		console.log("Database Error----------------", err);
-	}
-	console.log("Connected to database");
-});
+mongoose
+	.connect(process.env.CONNECTION_STRING, { useNewUrlParser: true })
+	.then(() => {
+		console.log("Database Connected");
+	})
+	.catch((err) => {
+		console.log("Cannot connect to the database: " + err);
+	});
 
 app.use(bodyParser.json());
 app.use(cors());
 
-//For storing cookies for the user.
+app.set("trust proxy", 1); // trust first proxy
 app.use(
 	session({
-		//Create a secret for the cookie store it in .env file
-		//Secret can be anything.
-		secret: process.env.SESSION_SECRET,
-		//this for resaving the cookie false, if true can cause a memory leak.
+		secret: "keyboard cat",
 		resave: false,
-		//saveUnitialized best false, unless connect to a database.
-		saveUninitialized: false,
-		cookie: {
-			//The max age of the cookie
-			maxAge: 1000 * 60 * 60 * 24 * 14
-		}
+		saveUninitialized: true,
+		cookie: { secure: true, maxAge: 1000 * 60 * 60 * 24 * 14 }
 	})
 );
 
 setTimeout(() => {
 	//All our endpoints.
 	//Read the user's session.
-	app.get("/api/user-data", userController.readUserData);
+	// app.get("/api/user-data", userController.readUserData);
 	//Add a item to cart.
-	app.post("/api/user-data/cart", userController.addToCart);
+	// app.post("/api/user-data/cart", userController.addToCart);
 	//Remove a item from the cart.
 	// Use request parameter to remove item from cart since you are looking a specific item in cart.
-	app.delete("/api/user-data/cart/:id", userController.removeFromCart);
+	// app.delete("/api/user-data/cart/:id", userController.removeFromCart);
 	//When user login
-	app.post("/api/login", userController.login);
+	// app.post("/api/login", userController.login);
 	//NO NEED FOR A REGISTER SINCE YOUR ARE USING AUTH0.
 	//Just need a login, since you are logging from your social media provider no need to register, only looks if a user already has a account.
 	//When the user logouts
-	app.post("/api/logout", userController.logout);
+	// app.post("/api/logout", userController.logout);
 	//Products Endpoints
 	//Getting all the products
 	app.get("/api/products", productsController.readAllProducts);
